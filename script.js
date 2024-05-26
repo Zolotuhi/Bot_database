551b04aab13c244f334856d8d347573cb2562358
Zolotuhi <156311422+Zolotuhi@users.noreply.github.com>
2024-05-26 21:30:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -26,38 +26,38 @@ function setLanguage(language) {
     fetchAttendanceData();
 }
 
-fetch('https://5b6389b0-984f-4896-abbd-bae6987a3853-00-nta4awm7pbls.sisko.repl.co/api/employees')
-    .then(response => response.json())
-    .then(data => {
-        const container = document.getElementById('attendance-container');
-        container.innerHTML = '';
+function fetchAttendanceData() {
+    fetch('https://5b6389b0-984f-4896-abbd-bae6987a3853-00-nta4awm7pbls.sisko.replit.dev:8080/api/employees')
+        .then(response => response.json())
+        .then(data => {
+            const container = document.getElementById('attendance-container');
+            container.innerHTML = '';
 
-        const table = document.createElement('table');
-        const thead = document.createElement('thead');
-        const tbody = document.createElement('tbody');
+            const table = document.createElement('table');
+            const thead = document.createElement('thead');
+            const tbody = document.createElement('tbody');
 
-        thead.innerHTML = `
-            <tr>
-                <th>${translations[currentLanguage].username}</th>
-                <th>${translations[currentLanguage].arrivalTime}</th>
-                <th>${translations[currentLanguage].departureTime}</th>
-            </tr>
-        `;
-
-        data.forEach(employee => {
-            const row = document.createElement('tr');
-            row.innerHTML = `
-                <td>${employee.username}</td>
-                <td>${employee.arrival_time}</td>
-                <td>${employee.departure_time}</td>
+            thead.innerHTML = `
+                <tr>
+                    <th>${translations[currentLanguage].username}</th>
+                    <th>${translations[currentLanguage].arrivalTime}</th>
+                    <th>${translations[currentLanguage].departureTime}</th>
+                </tr>
             `;
-            tbody.appendChild(row);
-        });
 
-        table.appendChild(thead);
-        table.appendChild(tbody);
-        container.appendChild(table);
-    })
-    .catch(error => console.error('Error fetching attendance data:', error));
+            data.forEach(employee => {
+                const row = document.createElement('tr');
+                row.innerHTML = `
+                    <td>${employee.username}</td>
+                    <td>${employee.arrival_time}</td>
+                    <td>${employee.departure_time}</td>
+                `;
+                tbody.appendChild(row);
+            });
 
-
+            table.appendChild(thead);
+            table.appendChild(tbody);
+            container.appendChild(table);
+        })
+        .catch(error => console.error('Error fetching attendance data:', error));
+}
